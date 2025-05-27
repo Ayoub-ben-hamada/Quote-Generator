4dfcb6b6690601364bb903f278475ceee0a78513
tibou-25 <ayoubbenhamada2@gmail.com>
2025-05-27 14:57:05 +0100

    final touches

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import QuoteGenerator from "./QuoteGenerator.jsx";
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <QuoteGenerator />
+    </div>
+  )
+}
+
+export default App
